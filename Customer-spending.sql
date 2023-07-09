520525bf270301989d5f97450a5503bd2d5ac28a
Utkarsh Singh <123075569+utkarsh0309@users.noreply.github.com>
2023-07-09 05:46:21 +0530

    Add files via upload

---
+++ Customer-spending.sql
@@ -0,0 +1,7 @@
+SELECT customer.customer_name,CAST(invoice.total_price AS DECIMAL(10,6)) 
+      Amount 
+    FROM customer 
+      INNER JOIN invoice ON invoice.customer_id=customer.id 
+    WHERE invoice.total_price<= (SELECT (0.25*AVG(invoice.total_price)) 
+    FROM invoice ) 
+    GROUP BY customer.id,customer.customer_name,invoice.total_price
