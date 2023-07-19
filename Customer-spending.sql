77a6ed9301837f681502947f0f348ce0a2587583
Utkarsh Singh <123075569+utkarsh0309@users.noreply.github.com>
2023-07-20 04:37:15 +0530

    Update Customer-spending.sql

--- Customer-spending.sql
+++ Customer-spending.sql
@@ -5,3 +5,5 @@ SELECT customer.customer_name,CAST(invoice.total_price AS DECIMAL(10,6))
     WHERE invoice.total_price<= (SELECT (0.25*AVG(invoice.total_price)) 
     FROM invoice ) 
     GROUP BY customer.id,customer.customer_name,invoice.total_price
+
+#mysql_solution
